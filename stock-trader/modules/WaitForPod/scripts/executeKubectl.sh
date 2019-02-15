187e24ad7e9429cb73cb8734ff315c15cf345684
bmarolleau <benoit.marolleau@fr.ibm.com>
2019-02-15 14:33:27 +0100

    Update executeKubectl.sh

--- stock-trader/modules/WaitForPod/scripts/executeKubectl.sh
+++ stock-trader/modules/WaitForPod/scripts/executeKubectl.sh
@@ -5,11 +5,22 @@
 
 #!/bin/bash
 namespace=$1
-args=$@
-#-- create --user team3 --password abc123 --release vision
-success="false"
+icpuser=$2
+icppassword=$3
+icpmaster=$4
+kubectlCmd=${@:5}
+kubectlCmd="-n $namespace $kubectlCmd"
 
-#`kubectl get pods -n ${namespace} > status.txt`
-kubectl $args
-#-n ${namespace} run  --rm -i --restart=Never usermgt --image=mycluster.icp:8500/ibmcom/powerai-vision-usermgt:1.1.2.0 $args
+#For AI Vision cmd lookis like 
+# run  --rm -i --restart=Never usermgt --image=mycluster.icp:8500/ibmcom/powerai-vision-usermgt:1.1.2.0 -- create --user team3 --password abc123 --release vision
+#/tmp/executeKubectl.sh default admin <password> mycluster.icp -n default run --rm -i --restart=Never usermgt --image=mycluster.icp:8500/ibmcom/powerai-vision-usermgt:1.1.2.0 -- list --release vision
+
+echo Target Namespace:$namespace
+echo ICP K8s credentials - ipcuser:$icpuser $icppassword $icpmaster
+echo Executing kubectl $kubectlCmd
+cloudargs="https://${icpmaster}:8443 -u ${icpuser} -p ${icppassword}  -n ${namespace}  --skip-ssl-validation"
+echo Cloudctl args: $cloudargs
+cloudctl login -a $cloudargs # https://${icpcmaster}:8443 -u ${icpuser} -p ${icppassword}  -n ${namespace}  --skip-ssl-validation
+kubectl $kubectlCmd
+
 
