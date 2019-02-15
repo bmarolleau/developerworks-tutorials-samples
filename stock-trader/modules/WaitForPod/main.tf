d8170f2a1d7c3c0fed420987c5e0c622c1d3e477
bmarolleau <benoit.marolleau@fr.ibm.com>
2019-02-15 14:53:18 +0100

    Update main.tf

--- stock-trader/modules/WaitForPod/main.tf
+++ stock-trader/modules/WaitForPod/main.tf
@@ -9,12 +9,21 @@ variable "password" {
   description = "ssh ibmadmin password"
 }
 
+variable "icp_username" {
+  description = "k8s icp admin username"
+}
+variable "icp_password" {
+  description = "admin icp password"
+}
+variable "icp_master_ip" {
+  description = "k8s master ip"
+}
 variable "namespace" {
   default     = "default"
   description = "namespace"
 }
 
-variable "args" {
+variable "kubectlargs" {
   default     = "-- create --user team3 --password abc123 --release vision"
   description = "command to manage paiv users"
 }
@@ -23,7 +32,7 @@ resource "null_resource" "ExecuteK8s" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/executeKubectl.sh",
-      "bash /tmp/executeKubectl.sh ${var.namespace} ${var.args}",
+      "bash /tmp/executeKubectl.sh ${var.namespace} ${var.icp_username} ${var.icp_password} ${var.icp_master_ip} ${var.kubectlargs}",
     ]
 
     connection {
